2cf7d8a56b0c716bc9575b930f196e3c9e8ba502
abdulmahl <mahalanguma87@gmail.com>
2023-11-19 18:00:15 +0200

    .

--- src/js/script.js
+++ src/js/script.js
@@ -4,6 +4,7 @@ import * as ReactDOM from "react-dom/client";
 import "react-quill/dist/quill.snow.css";
 import makeEditor from "../jsx/richTextEditor";
 
+
 makeEditor(".editor");
 
 // console.log(document.querySelector('.ql-font-size .ql-picker-options .ql-picker-item'))
