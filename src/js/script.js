426f8815e315111ddfcd348635d9c1db1ed61e53
Janalovelock <janalovelock@gmail.com>
2023-11-25 14:23:34 -0600

    updated crud

--- src/js/script.js
+++ src/js/script.js
@@ -3,8 +3,28 @@ import ReactQuill, { Quill } from "react-quill";
 import * as ReactDOM from "react-dom/client";
 import "react-quill/dist/quill.snow.css";
 import makeEditor from "../jsx/richTextEditor";
-import { addEntry, getAllEntries, updateEntry, deleteEntry } from "indexManager.js";
-
+import {
+    addProject,
+    getAllProjects,
+    updateProject,
+    deleteProject,
+    addListToProject,
+    getAllListsForProject,
+    updateList,
+    deleteListFromProject,
+    addCharacterToProject,
+    getAllCharactersForProject,
+    updateCharacter,
+    deleteCharacterFromProject,
+    addLocationToProject,
+    getAllLocationsForProject,
+    updateLocation,
+    deleteLocationFromProject,
+    addChapterToProject,
+    getAllChaptersForProject,
+    updateChapter,
+    deleteChapterFromProject
+  } from "indexedDBManager.js";
 
 makeEditor(".editor");
 
