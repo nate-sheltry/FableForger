d46778fa73074974303bb2b952f839e55ad3b0b2
abdulmahl <mahalanguma87@gmail.com>
2023-11-26 21:32:46 +0200

    .

--- src/js/uid.js
+++ src/js/uid.js
@@ -1,3 +1,4 @@
+//? Generate a unique id for each entry that we're gonna make!
 export const guid = () => {
     let uniqueId = parseInt(Math.random() * Number.MAX_SAFE_INTEGER);
     uniqueId = uniqueId.toString(18).slice(0, 5).padStart(5, "0").toLocaleUpperCase();
