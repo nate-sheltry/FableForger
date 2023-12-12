481fe5d3b359ec45950235ee52ca0f1f68d3f0c6
JordanPostak <jordan_postak@hotmail.com>
2023-12-12 10:01:25 -0700

    some css for small screen editor, still working on it

---
+++ src/js/toggleOutlineList.js
@@ -0,0 +1,7 @@
+document.getElementById('toggleOutlineBtn').addEventListener('click', function () {
+    document.querySelector('.toggleoutline').classList.toggle('show');
+  });
+
+document.getElementById('toggleListBtn').addEventListener('click', function () {
+    document.querySelector('.togglelist').classList.toggle('show');
+});
