167920d99d6f2e508888bcf756fb4b697b5c1e73
Abdul Mahl <mdu_abdul@icloud.com>
2023-12-13 11:31:00 +0200

    Fixed the list styling

--- src/js/toggleOutlineList.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('toggleOutlineBtn').addEventListener('click', function () {
-    document.querySelector('.toggleoutline').classList.toggle('show');
-  });
-
-document.getElementById('toggleListBtn').addEventListener('click', function () {
-    document.querySelector('.togglelist').classList.toggle('show');
-});
